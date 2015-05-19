882891d3c1cd95146d4c48ea4fa74441b3afc16b
Robin <542221324@qq.com>
2015-05-19 10:12:35 +0800

    add solution of string

--- string/main.cc
+++ string/main.cc
@@ -1,17 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "qh_string.h"
+#include <assert.h>
+#include <string.h>
+
+#define H_ARRAYSIZE(a) \
+	((sizeof(a) / sizeof(*(a))) / \
+	static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
+
+#define H_ARRAY_SIZE H_ARRAYSIZE
+
+void test1()                                  //测试构造函数
+{
+	const char* str = "Test of ctor";
+	qh::string myStr(str);
+	assert(!strcmp(myStr.data(), str));
+}
+
+void test2()				     //测试复制构造函数
+{
+	const char* str = "Test of assign ctor";
+	qh::string myStr(str);
+	qh::string myStr1(myStr);
+	assert(!strcmp(myStr1.data(), str));
+}
+
+void test3()									
+{
+	char* str = "Test of qh::string(const char*, size_t)";
+	qh::string myStr(str + 12, 27);
+	assert(!strcmp(myStr.data(), &str[12]));
+}
+
+void test4()				     //测试operator[]
+{
+	char* str = "Test of operator []";
+	qh::string myStr(str);
+	for (size_t i = 0; i < myStr.size(); ++i)
+	{
+		assert(myStr[i] == str[i]);
+	}	
+}
+
+void test5()				     //测试赋值操作符
+{
+	const char* str = "Test of operator =";
+	qh::string myStr(str);
+	qh::string myStr1(&str[5]);
+	myStr1 = myStr;
+	assert(!strcmp(myStr1.data(), str));
+}
 
 
 int main(int argc, char* argv[])
 {
-    //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
-    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	//TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+	//TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
 
 #ifdef WIN32
-    system("pause");
+	system("pause");
 #endif
-
 	return 0;
 }
-
--- string/qh_string.cc
+++ string/qh_string.cc
@@ -1,5 +1,5 @@
 #include "qh_string.h"
-
+#include <stdio.h>
 #include <string.h>
 
 namespace qh
@@ -7,31 +7,78 @@ namespace qh
     // TODO 将在这里写实现代码
 
     string::string()
-        : data_(NULL), len_(0)
+        : len_(0)
     {
+		data_ = new char[1];
+		data_[0] = '\0';
     }
 
     string::string( const char* s )
     {
+		if(s == NULL)
+		{
+			data_ = new char[1];
+			data_[0] = '\0';
+			len_ = 0;
+		}
+		else
+		{
+			len_ = strlen(s);
+			data_ = new char[len_ + 1];
+			strcpy(data_, s);	
+		}	
     }
 
     string::string( const char* s, size_t len )
     {
+		if(s == NULL || len < 1)
+		{
+			data_ = new char[1];
+			data_[0] = '\0';
+			len_ = 0;
+		}
+		else if(len >= strlen(s))
+		{
+			len_ = strlen(s);
+			data_ = new char[len_ + 1];
+			strcpy(data_, s);	
+		}
+		else
+		{
+			data_ = new char[len + 1];
+			len_ = len;
+			int i = 0;
+			char* curpos = data_;
+			while (i++ < len)
+				*curpos++ = *s++;
+			*curpos = '\0';
+		}	
     }
 
     string::string( const string& rhs )
     {
-
+		len_ = rhs.len_;
+		data_ = new char[len_ + 1];
+		strcpy(data_, rhs.data());
     }
 
     string& string::operator=( const string& rhs )
     {
-        return *this;
+        if(this == &rhs) return *this;
+
+		char* tmp_data = data_;
+
+		data_ = new char[rhs.len_ + 1];
+		strcpy(data_, rhs.data_);
+		len_ = rhs.len_;
+
+		delete[] tmp_data;
+		return *this;
     }
 
     string::~string()
     {
-
+		delete[] data_;
     }
 
     size_t string::size() const
@@ -41,16 +88,32 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
-    char* string::operator[]( size_t index )
+    
+	char& string::operator[]( size_t index )
+	{
+		if(index >= len_)
+		{
+			fprintf(stderr, "index of string out of rage.\n");
+			exit(1);
+		}
+		return data_[index];
+	}
+
+	const char& string::operator[]( size_t index ) const
     {
-        return NULL;
+        if(index >= len_)
+		{
+			fprintf(stderr, "index of string out of rage.\n");
+			exit(1);
+		}
+		return data_[index];
     }
 }
