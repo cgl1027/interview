d443f9caefe20b047693e80995390a8db8de4b5d
Robin <542221324@qq.com>
2015-05-19 10:27:01 +0800

    add solution of vector

--- vector/main.cc
+++ vector/main.cc
@@ -1,17 +1,110 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <assert.h>
 #include "qh_vector.h"
 
+void test1()                                    //测试构造函数
+{
+	std::string str = "Test of function vector(size_t, const T&)";
+	qh::vector<std::string> myVec(10, str);
+	qh::vector<std::string> myVec1(myVec);
+	assert(myVec.size() == 10);
+	for (size_t i = 0; i < myVec.size(); ++i)
+	{
+		assert(myVec[i] == str);
+	}	
+	assert(myVec1.size() == 10);
+	for (size_t i = 0; i < myVec1.size(); ++i)
+	{
+		assert(myVec1[i] == str);
+	}	
+}
+
+void test2()                                  //测试赋值操作
+{
+	std::string str = "Test of operator = ";
+	qh::vector<std::string> myVec(10, str);
+	qh::vector<std::string> myVec1;
+	qh::vector<std::string> myVec2;
+	myVec2 = myVec1 = myVec;
+	assert(myVec1.size() == 10);
+	assert(myVec2.size() == 10);
+	for (size_t i = 0; i < myVec.size(); ++i)
+	{
+		assert(myVec1[i] == str);
+		assert(myVec2[i] == str);
+	}	
+}
+
+void test3()                                 //测试push_back函数
+{
+	int num[] = {1,2,3,4,5,6,7,8};
+	qh::vector<int> num_vec;
+	for(size_t i = 0; i < 8; ++i)
+		num_vec.push_back(num[i]);
+	assert(num_vec.size() == 8);
+	for(int i = 0; i < 8; ++i)
+		assert(num_vec[i] == num[i]);
+}
+
+void test4()                                 //测试利用vector<vector<int>>生成二维数组
+{
+	int num[] = {1,2,3,4,5,6,7,8};
+	qh::vector<int> num_vec;
+	qh::vector<qh::vector<int> > numVec_vec;
+	for(int i = 0; i < 8; ++i)
+		num_vec.push_back(num[i]);
+	for (int i = 0; i < 3; ++i)
+		numVec_vec.push_back(num_vec);
+	assert(numVec_vec.size() == 3);
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 8; ++j)
+			assert(numVec_vec[i][j] == num[j]);	
+}
+
+void test5()                                  //测试pop_back函数
+{
+	int num[] = {1,2,3,4,5,6,7,8};
+	qh::vector<int> num_vec;
+	for(int i = 0; i < 8; ++i)
+		num_vec.push_back(num[i]);
+	for(int i = 0; i < 8; ++i)
+	{
+		num_vec.pop_back();
+		assert(num_vec.size() == 8 - i - 1);
+		for (size_t j = 0; j < num_vec.size(); ++j)
+			assert(num_vec[j] == num[j]);	
+	}
+}
+
+void test6()                                   //测试resize函数
+{
+	int num[] = {1,2,3,4,5,6,7,8};
+	int numResize[] = {1,2,3,4,5,6,7,8,0,0,0,0};
+	qh::vector<int> num_vec;
+	for(int i = 0; i < 8; ++i)
+		num_vec.push_back(num[i]);
+	for (int i = 12; i >= 0; --i)
+	{
+		num_vec.resize(i);
+		for (size_t j = 0; j < num_vec.size(); ++j)
+			assert(num_vec[j] == numResize[j]);	
+	}
+}
 
 int main(int argc, char* argv[])
 {
-    //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
-    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
-
-    qh::vector<int> num_vect;
-    num_vect.push_back(1);
-
+	//TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+	//TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
+	test6();
 #ifdef WIN32
-    system("pause");
+	system("pause");
 #endif
 
 	return 0;
--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -8,32 +8,38 @@ namespace qh
     public:
         //ctor
         vector()
-            : data_(NULL), size_(0)
+            : data_(NULL), size_(0), capacity_(0)
         {
         }
 
-        explicit vector( size_t n, const T& value = T())
+        vector( size_t n, const T& value = T())
         {
-            data_ = new T[n];
+            capacity_ = DoubleSize(n);
+			data_ = new T[capacity_];
             for (size_t i = 0; i < n; i++)
             {
-                data_[0] = value;
+                data_[i] = value;
             }
+			size_ = n;
         }
 
-        vector<T>& operator=(const vector<T>& rhs)
-        {
-            data_ = rhs.data_;
-            size_ = rhs.size_;
-        }
+		vector(vector<T>& rhs)
+		{
+			capacity_ = rhs.capacity_;
+			data_ = new T[capacity_];
+			for (size_t i = 0; i < rhs.size_; i++)
+			{
+				data_[i] = rhs[i];
+			}
+			size_ = rhs.size_;
+		}
+
+		vector& operator=(const vector& rhs);
 
         //dtor
         ~vector()
         {
-            if (data_)
-            {
-                delete data_;
-            }
+			delete[] data_;
         }
 
         //get
@@ -43,20 +49,122 @@ namespace qh
         }
 
         // set & get
-        T& operator[](size_t index);
+        T& operator[](size_t index) const;
 
         // set
         void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize();
-        void reserve();
+        void pop_back();
+        void resize(size_t n, const T& element = T());
+        void reserve(size_t n);
         void clear();
-        void empty();
+        void empty() {return size_ == 0;}
 
     private:
         T*      data_;
-        size_t  size_;
+        size_t  size_;                          //data_的有效数据大小
+		size_t  capacity_;		//data_的实际容量大小
+
+		size_t DoubleSize(size_t s)     //用于计算capacity_, capacity_总是大于等于有效数据个数的
+		{				//2的整数幂，从而防止push_back()时频繁复制数据
+			size_t res = 0x1;
+			while(res < s) res <<= 1;
+			return res;
+		}
     };
+
+	template<class T>
+	vector<T>& vector<T>::operator=(const vector<T>& rhs)
+	{
+		if(this == &rhs) return *this;
+		delete[] data_;
+		data_ = new T[rhs.capacity_];
+		size_ = rhs.size_;
+		capacity_ = rhs.capacity_;
+		for (size_t i = 0; i < rhs.size_; i++)
+		{
+			(*this)[i] = rhs[i];
+		}
+		return *this;
+	}
+
+	template<class T>
+	T& vector<T>::operator[](size_t index) const
+	{
+		if(index >= size_)
+		{
+			fprintf(stderr, "index is out of range.\n");
+			exit(1);
+		}
+		return data_[index];
+	}
+
+	template<class T>
+	void vector<T>::push_back(const T& element)
+	{
+		if (size_ == capacity_)							
+		{												
+			T* tep_data = data_;
+
+			capacity_ = DoubleSize(size_+1);	
+			data_ = new T[capacity_];
+
+			for (size_t i = 0; i < size_; ++i)
+				data_[i] = tep_data[i];
+
+			data_[size_] = element;
+			++size_;
+			delete[] tep_data;
+		}
+		else											
+		{
+			data_[size_] = element;
+			++size_;
+		}
+	}
+
+
+	template<class T>
+	void vector<T>::pop_back()
+	{
+		if(size_ > 0)  --size_;			
+		if(size_ == 0) clear();		        //如果删除后data_为空，则运行clear()
+	}
+
+	template<class T>
+	void vector<T>::resize(size_t n, const T& element)
+	{
+		if(n > size_)
+		{
+			for (size_t i = size_; i < n; ++i)
+			{
+				push_back(element);
+			}
+		}
+		size_ = n;
+	}
+
+	template<class T>
+	void vector<T>::reserve(size_t n)
+	{
+		if(n > capacity_)
+		{
+			T* tep_data = data_;
+			data_ = new T[n];
+			capacity_ = n;
+			for(int i = 0; i < size_; ++i)
+				data_[i] = tep_data[i];
+			delete[] tep_data;
+		}
+	}
+
+	template<class T>
+	void vector<T>::clear()
+	{
+		delete[] data_;
+		data_ = NULL;
+		size_ = 0;
+		capacity_ = 0;
+	}
 }
 
 #endif
